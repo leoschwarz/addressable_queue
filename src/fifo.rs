003d2e4e7f8af19874e688d94512cd57f553c24c
Leo Schwarz <mail@leoschwarz.com>
2018-03-02 14:35:48 +0100

    Add tests.

--- src/fifo.rs
+++ src/fifo.rs
@@ -46,6 +46,20 @@ where
     }
 
     /// Insert an entry at the end of the queue.
+    ///
+    /// ```
+    /// use addressable_queue::fifo::Queue;
+    ///
+    /// let mut queue = Queue::new();
+    /// queue.insert(2u8, 4u8);
+    /// queue.insert(3u8, 6u8);
+    /// queue.insert(4u8, 8u8);
+    ///
+    /// assert_eq!(Some((2, 4)), queue.remove_head());
+    /// assert_eq!(Some((3, 6)), queue.remove_head());
+    /// assert_eq!(Some((4, 8)), queue.remove_head());
+    /// assert_eq!(None, queue.remove_head());
+    /// ```
     pub fn insert(&mut self, key: K, value: V) {
         let arc = Arc::new(Item {
             key: key.clone(),
@@ -55,10 +69,24 @@ where
         self.pointers.insert(key, arc);
     }
 
-    /// Insert an entry at the beginning of the queue.
+    /// Insert an entry at the front of the queue.
     ///
     /// This is mostly useful when removing the head and
     /// then deciding to put it back into the queue.
+    ///
+    /// ```
+    /// use addressable_queue::fifo::Queue;
+    ///
+    /// let mut queue = Queue::new();
+    /// queue.insert_head(2u8, 4u8);
+    /// queue.insert_head(3u8, 6u8);
+    /// queue.insert_head(4u8, 8u8);
+    ///
+    /// assert_eq!(Some((4, 8)), queue.remove_head());
+    /// assert_eq!(Some((3, 6)), queue.remove_head());
+    /// assert_eq!(Some((2, 4)), queue.remove_head());
+    /// assert_eq!(None, queue.remove_head());
+    /// ```
     pub fn insert_head(&mut self, key: K, value: V) {
         let arc = Arc::new(Item {
             key: key.clone(),
@@ -69,40 +97,89 @@ where
     }
 
     /// Remove the current head of the queue, and return the value if there was one.
-    pub fn remove_head(&mut self) -> Option<V> {
+    ///
+    /// ```
+    /// use addressable_queue::fifo::Queue;
+    ///
+    /// let mut queue = Queue::new();
+    /// queue.insert(2u8, 4u8);
+    /// queue.insert(3u8, 6u8);
+    /// queue.insert(4u8, 8u8);
+    ///
+    /// assert_eq!(Some((2, 4)), queue.remove_head());
+    /// assert_eq!(Some((3, 6)), queue.remove_head());
+    /// assert_eq!(Some((4, 8)), queue.remove_head());
+    /// assert_eq!(None, queue.remove_head());
+    /// ```
+    pub fn remove_head(&mut self) -> Option<(K, V)> {
         while let Some(item) = self.items.pop_front() {
             let is_some = item.val.lock().unwrap().is_some();
             if is_some {
                 self.pointers.remove(&item.key);
-                return Arc::try_unwrap(item)
+                let key = item.key.clone();
+                let value = Arc::try_unwrap(item)
                     .ok()
                     .unwrap()
                     .val
                     .into_inner()
+                    .unwrap()
                     .unwrap();
+                return Some((key, value));
             }
         }
         None
     }
 
     /// Remove the current tail of the queue, and return the value if there was one.
-    pub fn remove_tail(&mut self) -> Option<V> {
+    ///
+    /// ```
+    /// use addressable_queue::fifo::Queue;
+    ///
+    /// let mut queue = Queue::new();
+    /// queue.insert(2u8, 4u8);
+    /// queue.insert(3u8, 6u8);
+    /// queue.insert(4u8, 8u8);
+    ///
+    /// assert_eq!(Some((4, 8)), queue.remove_tail());
+    /// assert_eq!(Some((3, 6)), queue.remove_tail());
+    /// assert_eq!(Some((2, 4)), queue.remove_tail());
+    /// assert_eq!(None, queue.remove_tail());
+    /// ```
+    pub fn remove_tail(&mut self) -> Option<(K, V)> {
         while let Some(item) = self.items.pop_back() {
             let is_some = item.val.lock().unwrap().is_some();
             if is_some {
                 self.pointers.remove(&item.key);
-                return Arc::try_unwrap(item)
+                let key = item.key.clone();
+                let value = Arc::try_unwrap(item)
                     .ok()
                     .unwrap()
                     .val
                     .into_inner()
+                    .unwrap()
                     .unwrap();
+                return Some((key, value));
             }
         }
         None
     }
 
     /// Remove a value by specifying its key.
+    ///
+    /// ```
+    /// use addressable_queue::fifo::Queue;
+    ///
+    /// let mut queue = Queue::new();
+    /// queue.insert(2u8, 4u8);
+    /// queue.insert(3u8, 6u8);
+    /// queue.insert(4u8, 8u8);
+    ///
+    /// assert_eq!(Some(4), queue.remove_key(&2));
+    /// assert_eq!(Some(6), queue.remove_key(&3));
+    /// assert_eq!(None, queue.remove_key(&3));
+    /// assert_eq!(Some(8), queue.remove_key(&4));
+    /// assert_eq!(None, queue.remove_head());
+    /// ```
     pub fn remove_key(&mut self, key: &K) -> Option<V> {
         if let Some(item) = self.pointers.remove(key) {
             let mut val = None;
@@ -111,4 +188,15 @@ where
         }
         None
     }
+
+    /*
+    /// Convert the queue into a vec, where the first element is the head (oldest element).
+    pub fn into_vec(self) -> Vec<(K,V)> {
+        let mut vec = Vec::new();
+        while let Some(item) = self.remove_head() {
+            vec.push(item);
+        }
+        vec
+    }
+    */
 }
